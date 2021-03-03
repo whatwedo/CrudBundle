d0f8ac69c70b95d32c73ad39b2a835fb41b09e3f
Bert-Jan Fikse <bert-jan@whatwedo.ch>
2021-03-03 13:33:45 +0100

    fix(EntityAjaxType): change ChoiceLoader class

--- Form/Type/EntityAjaxType.php
+++ Form/Type/EntityAjaxType.php
@@ -27,7 +27,6 @@
 
 namespace whatwedo\CrudBundle\Form\Type;
 
-use Symfony\Bridge\Doctrine\Form\ChoiceList\DoctrineChoiceLoader;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -38,6 +37,7 @@ use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\RouterInterface;
 use whatwedo\CrudBundle\Form\ChoiceLoader\AjaxDoctrineChoiceLoader;
+use Symfony\Component\Form\ChoiceList\Factory\Cache\ChoiceLoader;
 
 class EntityAjaxType extends AbstractType
 {
@@ -71,7 +71,7 @@ class EntityAjaxType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefault('choice_loader', function (Options $options, DoctrineChoiceLoader $doctrineChoiceLoader) {
+        $resolver->setDefault('choice_loader', function (Options $options, ChoiceLoader $doctrineChoiceLoader) {
             if ($doctrineChoiceLoader) {
                 return new AjaxDoctrineChoiceLoader($doctrineChoiceLoader);
             }
