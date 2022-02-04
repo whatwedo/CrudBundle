baa41f3fb18d8d766f7c0126c8480efd59ea9b9b
Maurizio Monticelli <rugbymauri@gmail.com>
2022-02-04 14:32:04 +0100

    feat: Modal Action

---
+++ Action/ModalAction.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace whatwedo\CrudBundle\Action;
+
+use Exception;
+use InvalidArgumentException;
+use Symfony\Component\Form\Util\StringUtil;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use whatwedo\CrudBundle\Enum\Page;
+use whatwedo\CrudBundle\Enum\VisibilityEnum;
+
+class ModalAction extends Action
+{
+}
