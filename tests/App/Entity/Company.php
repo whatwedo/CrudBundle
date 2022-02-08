cf42c784b4d0f58eda547c25eda6d4c5b49d8905
Maurizio Monticelli <rugbymauri@gmail.com>
2022-02-08 15:54:50 +0100

    test: add config for search bundle

--- tests/App/Entity/Company.php
+++ tests/App/Entity/Company.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
-use whatwedo\CrudBundle\Annotation\Index;
+use whatwedo\SearchBundle\Annotation\Index;
 
 /**
  * @ORM\Table(name="company")
