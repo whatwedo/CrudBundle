fa6c32be7c8776465256b49d9038c48fe192ef48
tuxes3 <nicolo@whatwedo.ch>
2022-03-24 13:11:04 -0500

    feat: added crud render extension

--- src/Twig/CrudRenderExtension.php
+++ src/Twig/CrudRenderExtension.php
@@ -11,8 +11,12 @@ use Twig\TwigFunction;
 use whatwedo\CoreBundle\Action\Action;
 use whatwedo\CoreBundle\Manager\FormatterManager;
 use whatwedo\CrudBundle\Block\Block;
+use whatwedo\CrudBundle\Block\DefinitionBlock;
 use whatwedo\CrudBundle\Content\AbstractContent;
+use whatwedo\CrudBundle\Enum\Page;
 use whatwedo\CrudBundle\Enum\PageInterface;
+use whatwedo\CrudBundle\Exception\BlockNotFoundException;
+use whatwedo\CrudBundle\Manager\DefinitionManager;
 use whatwedo\CrudBundle\View\DefinitionView;
 
 class CrudRenderExtension extends AbstractExtension
@@ -20,6 +24,8 @@ class CrudRenderExtension extends AbstractExtension
     public function __construct(
         protected Environment $environment,
         protected FormatterManager $formatterManager,
+        protected DefinitionManager $definitionManager,
+        protected Environment $twig
     ) {
     }
 
@@ -34,6 +40,7 @@ class CrudRenderExtension extends AbstractExtension
 
         return [
             new TwigFunction('wwd_crud_render_block', fn ($context, Block $block, DefinitionView $view, PageInterface $page, ?FormView $form = null) => $this->renderBlock($context, $block, $view, $page, $form), $options),
+            new TwigFunction('wwd_definition_block_render', fn ($context, DefinitionBlock $definitionBlock) => $this->renderDefinitionBlock($context, $definitionBlock), $options),
             new TwigFunction('wwd_crud_render_content', fn ($context, $content, Block $block, DefinitionView $view, ?FormView $form = null) => $this->renderContent($context, $content, $block, $view, $form), $options),
             new TwigFunction('wwd_crud_render_action', fn ($context, Action $action, DefinitionView $view, ?FormView $form = null) => $this->renderAction($context, $action, $view), $options),
             new TwigFunction('wwd_crud_render_content_value', fn ($context, AbstractContent $content) => $this->renderContentValue($context, $content), $options),
@@ -109,5 +116,30 @@ class CrudRenderExtension extends AbstractExtension
         }
 
         return (string) $data;
+    }
+
+    public function renderDefinitionBlock($context, DefinitionBlock $definitionBlock): string
+    {
+        $data = $definitionBlock->getData($context['view']->getData());
+        if ($data === null) {
+            return '';
+        }
+        $optionDefinition = $definitionBlock->getOption('definition');
+        $optionBlock = $definitionBlock->getOption('block');
+        if ($optionDefinition === null) {
+            $definition = $this->definitionManager->getDefinitionByEntity($data);
+        } else {
+            $definition = $this->definitionManager->getDefinitionByClassName($optionDefinition);
+        }
+        $view = $definition->createView(Page::SHOW, $data);
+        $block = $view->getBlocks(Page::SHOW)->filter(static fn (Block $block) => $block->getAcronym() === $optionBlock)->first();
+        if ($block === false) {
+            throw new BlockNotFoundException('Block "'.$optionBlock.'" does not exist in definition "'.get_class($definition).'".');
+        }
+        $template = $this->twig->load($definition->getTemplateDirectory().'show.html.twig');
+        return $template->renderBlock('block_definition_single_block', [
+            'view' => $view,
+            'block' => $block,
+        ]);
     }
 }
