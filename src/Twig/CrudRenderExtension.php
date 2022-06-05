b08b67dfac004a3039d60a8b0effd395f2c50257
Maurizio Monticelli <rugbymauri@gmail.com>
2022-06-05 23:21:04 +0200

    feat: pass page to teig functions

--- src/Twig/CrudRenderExtension.php
+++ src/Twig/CrudRenderExtension.php
@@ -12,6 +12,7 @@ use whatwedo\CoreBundle\Action\Action;
 use whatwedo\CoreBundle\Manager\FormatterManager;
 use whatwedo\CrudBundle\Block\Block;
 use whatwedo\CrudBundle\Content\AbstractContent;
+use whatwedo\CrudBundle\Enum\PageInterface;
 use whatwedo\CrudBundle\View\DefinitionView;
 
 class CrudRenderExtension extends AbstractExtension
@@ -32,14 +33,14 @@ class CrudRenderExtension extends AbstractExtension
         ];
 
         return [
-            new TwigFunction('wwd_crud_render_block', fn ($context, Block $block, DefinitionView $view, ?FormView $form = null) => $this->renderBlock($context, $block, $view, $form), $options),
+            new TwigFunction('wwd_crud_render_block', fn ($context, Block $block, DefinitionView $view, PageInterface $page, ?FormView $form = null) => $this->renderBlock($context, $block, $view, $page, $form), $options),
             new TwigFunction('wwd_crud_render_content', fn ($context, $content, Block $block, DefinitionView $view, ?FormView $form = null) => $this->renderContent($context, $content, $block, $view, $form), $options),
             new TwigFunction('wwd_crud_render_action', fn ($context, Action $action, DefinitionView $view, ?FormView $form = null) => $this->renderAction($context, $action, $view), $options),
             new TwigFunction('wwd_crud_render_content_value', fn ($context, AbstractContent $content) => $this->renderContentValue($context, $content), $options),
         ];
     }
 
-    protected function renderBlock($context, Block $block, DefinitionView $view, ?FormView $form): string
+    protected function renderBlock($context, Block $block, DefinitionView $view, PageInterface $page, ?FormView $form): string
     {
         $template = $this->environment->load($view->getDefinition()->getLayout());
 
@@ -49,6 +50,7 @@ class CrudRenderExtension extends AbstractExtension
             'view' => $view,
             'block' => $block,
             'form' => $form,
+            'page' => $page,
         ];
 
         return $template->renderBlock(
