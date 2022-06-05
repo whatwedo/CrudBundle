230d1957b66d9a3dba444c91501ec68cac1dc136
Maurizio Monticelli <rugbymauri@gmail.com>
2022-06-05 16:32:39 +0200

    feat: render functions for edit end create

--- src/Twig/CrudRenderExtension.php
+++ src/Twig/CrudRenderExtension.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace whatwedo\CrudBundle\Twig;
 
+use Symfony\Component\Form\FormView;
 use Twig\Environment;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 use whatwedo\CoreBundle\Manager\FormatterManager;
 use whatwedo\CrudBundle\Block\Block;
 use whatwedo\CrudBundle\Content\AbstractContent;
+use whatwedo\CrudBundle\View\DefinitionView;
 
 class CrudRenderExtension extends AbstractExtension
 {
@@ -29,37 +31,50 @@ class CrudRenderExtension extends AbstractExtension
         ];
 
         return [
-            new TwigFunction('wwd_crud_render_block', fn ($context, Block $block) => $this->renderBlock($context, $block), $options),
-            new TwigFunction('wwd_crud_render_content', fn ($context, AbstractContent $content, Block $block) => $this->renderContent($context, $content, $block), $options),
-            new TwigFunction('wwd_crud_render_content_value', fn ($context, AbstractContent $content) => $this->renderContentItem($context, $content), $options),
+            new TwigFunction('wwd_crud_render_block', fn ($context, Block $block, DefinitionView $view, ?FormView $form = null) => $this->renderBlock($context, $block, $view, $form), $options),
+            new TwigFunction('wwd_crud_render_content', fn ($context, $content, Block $block, DefinitionView $view, ?FormView $form = null) => $this->renderContent($context, $content, $block, $view, $form), $options),
+            new TwigFunction('wwd_crud_render_content_value', fn ($context, AbstractContent $content) => $this->renderContentValue($context, $content), $options),
         ];
     }
 
-    protected function renderBlock($context, Block $block): string
+    protected function renderBlock($context, Block $block, DefinitionView $view, ?FormView $form): string
     {
-        return $this->environment->render(
-            '/whatwedoCrud/includes/layout/_block.html.twig',
-            [
-                'view' => $context['view'],
-                'block' => $block,
-            ]
+        $template = $this->environment->load($view->getDefinition()->getLayout());
+
+        $blockName = $block->getOption('block_prefix');
+
+        $renderContext = [
+            'view' => $view,
+            'block' => $block,
+            'form' => $form,
+        ];
+
+        return $template->renderBlock(
+            $blockName,
+            $renderContext
         );
     }
 
-    protected function renderContent($context, AbstractContent $content, Block $block): string
+    protected function renderContent($context, $content, Block $block, DefinitionView $view, ?FormView $form): string
     {
-        $template = $this->environment->load('/whatwedoCrud/includes/layout/_content.html.twig');
+        $template = $this->environment->load($view->getDefinition()->getLayout());
 
         $blockName = $content->getBlockPrefix();
 
-        return $template->renderBlock($blockName, [
+        $renderContext = [
             'view' => $context['view'],
             'block' => $block,
             'content' => $content,
-        ]);
+        ];
+
+        if ($form) {
+            $renderContext['form'] = $form;
+        }
+
+        return $template->renderBlock($blockName, $renderContext);
     }
 
-    protected function renderContentItem($context, AbstractContent $content): string
+    protected function renderContentValue($context, AbstractContent $content): string
     {
         $data = $content->getContents($context['view']->getData());
         $formatter = $content->getOption('formatter');
