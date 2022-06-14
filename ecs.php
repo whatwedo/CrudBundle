dc544a4efb00447843b70085f91e7867f6c037cf
tuxes3 <nicolo.singer@gmail.com>
2022-06-14 15:30:52 +0200

    chore: new ECS Config style

--- ecs.php
+++ ecs.php
@@ -6,13 +6,15 @@ use PHP_CodeSniffer\Standards\Squiz\Sniffs\Classes\ValidClassNameSniff;
 use PHP_CodeSniffer\Standards\Squiz\Sniffs\Commenting\ClassCommentSniff;
 use PHP_CodeSniffer\Standards\Squiz\Sniffs\Commenting\FileCommentSniff;
 use PHP_CodeSniffer\Standards\Squiz\Sniffs\Commenting\FunctionCommentThrowTagSniff;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer;
+use PhpCsFixer\Fixer\Whitespace\ArrayIndentationFixer;
+use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayListItemNewlineFixer;
+use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayOpenerAndCloserNewlineFixer;
+use Symplify\CodingStandard\Fixer\ArrayNotation\StandaloneLineInMultilineArrayFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ECSConfig $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
+    $containerConfigurator->paths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
@@ -20,7 +22,7 @@ return static function (ECSConfig $containerConfigurator): void {
     // run and fix, one by one
     $containerConfigurator->import('vendor/whatwedo/php-coding-standard/config/whatwedo-symfony.php');
 
-    $containerConfigurator->parameters()->set(Option::SKIP, [
+    $containerConfigurator->skip([
         FileCommentSniff::class,
         ClassCommentSniff::class,
         FunctionCommentThrowTagSniff::class,
@@ -28,23 +30,22 @@ return static function (ECSConfig $containerConfigurator): void {
             __DIR__ . '/src/whatwedoCrudBundle.php',
             __DIR__ . '/src/DependencyInjection/whatwedoCrudExtension.php',
         ],
-        \PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer::class => [
+        TrailingCommaInMultilineFixer::class => [
             __DIR__ . '/tests/App/config/bundles.php',
         ],
-        \PhpCsFixer\Fixer\Whitespace\ArrayIndentationFixer::class => [
+        ArrayIndentationFixer::class => [
             __DIR__ . '/tests/App/config/bundles.php',
         ],
-        \Symplify\CodingStandard\Fixer\ArrayNotation\ArrayListItemNewlineFixer::class => [
+        ArrayListItemNewlineFixer::class => [
             __DIR__ . '/tests/App/config/bundles.php',
         ],
-        \Symplify\CodingStandard\Fixer\ArrayNotation\StandaloneLineInMultilineArrayFixer::class => [
+        StandaloneLineInMultilineArrayFixer::class => [
             __DIR__ . '/tests/App/config/bundles.php',
         ],
-
-        \Symplify\CodingStandard\Fixer\ArrayNotation\ArrayOpenerAndCloserNewlineFixer::class => [
+        ArrayOpenerAndCloserNewlineFixer::class => [
             __DIR__ . '/tests/App/config/bundles.php',
         ],
     ]);
 
-    $parameters->set(Option::PARALLEL, true);
+    $containerConfigurator->parallel();
 };
