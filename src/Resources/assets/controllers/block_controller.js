91ff47ccfbaba5a6f507b9a50b05979841e3bb9d
tuxes3 <nicolo.singer@gmail.com>
2022-06-23 17:14:36 +0200

    wiop

--- src/Resources/assets/controllers/block_controller.js
+++ src/Resources/assets/controllers/block_controller.js
@@ -8,9 +8,11 @@ export default class extends Controller {
     }
 
     connect() {
-        if (this.collapsibleValue) {
+        if (this.collapsibleValue && this.hasTitleDivTarget && this.hasBlockDivTarget) {
             console.log(this.titleDivTarget);
             console.log(this.blockDivTarget);
+        } else {
+            console.log('not collapsible');
         }
     }
 }
