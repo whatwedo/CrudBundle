a2cff175fb5e81ce6459b80f4ba9254762237f4a
tuxes3 <nicolo.singer@gmail.com>
2022-07-05 17:05:58 +0200

    feat(collapsible): removed "Div" suffix in block_controller.js

--- src/Resources/assets/controllers/block_controller.js
+++ src/Resources/assets/controllers/block_controller.js
@@ -1,20 +1,20 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ['titleDiv', 'blockDiv'];
+    static targets = ['title', 'block'];
 
     static values = {
         collapsible: Boolean
     }
 
     connect() {
-        if (this.collapsibleValue && this.hasTitleDivTarget && this.hasBlockDivTarget) {
-            this.titleDivTarget.style.cursor = 'pointer';
-            this.titleDivTarget.addEventListener('click', this.toggleBlock.bind(this));
+        if (this.collapsibleValue && this.hasTitleTarget && this.hasBlockTarget) {
+            this.titleTarget.style.cursor = 'pointer';
+            this.titleTarget.addEventListener('click', this.toggleBlock.bind(this));
         }
     }
 
     toggleBlock() {
-        this.blockDivTarget.classList.toggle('hidden');
+        this.blockTarget.classList.toggle('hidden');
     }
 }
