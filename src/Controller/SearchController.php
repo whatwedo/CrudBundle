1e1f8d036919307d3a92b2c5ee001ff5d636b4fb
Markus Boss <markus@whatwedo.ch>
2022-12-14 17:35:16 +0100

    refacter: code review

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -1,19 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace whatwedo\CrudBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use whatwedo\SearchBundle\Manager\SearchManager;
 use whatwedo\SearchBundle\Trait\SearchTrait;
 
 class SearchController extends AbstractController
 {
     use SearchTrait;
-    
-    public function search(Request $request, SearchManager $searchManager): Response
+
+    public function searchAction(Request $request, SearchManager $searchManager): Response
     {
         $templateParams = $this->getGlobalResults($request, $searchManager);
 
