7523096b348c40e833fc5461cee61da019eeee67
Arno Egli <arno@whatwedo.ch>
2026-03-11 23:43:00 +0100

    feat(routing): support legacy 'crud' type for per-definition route loading
    Add backward-compatible support for the old 'type: crud' routing format
    that loads routes for a single definition class. The new 'type:
    whatwedo_crud'
    loads all definitions at once. Both coexist without conflict.
    
    Signed-off-by: Arno Egli <arno@whatwedo.ch>

--- src/Routing/CrudLoader.php
+++ src/Routing/CrudLoader.php
@@ -22,80 +22,103 @@ class CrudLoader extends Loader
 
     public function load(mixed $resource, mixed $type = null): RouteCollection
     {
-        if ($this->isLoaded) {
-            throw new \RuntimeException('Do not add the "whatwedo_crud" loader twice');
+        if ($type === 'whatwedo_crud') {
+            if ($this->isLoaded) {
+                throw new \RuntimeException('Do not add the "whatwedo_crud" loader twice');
+            }
+            $this->isLoaded = true;
+            return $this->loadAllDefinitions($resource);
         }
 
+        // type === 'crud': load a single definition by class name
+        if (is_string($resource) && class_exists($resource)) {
+            return $this->loadDefinition($resource, $resource);
+        }
+
+        return new RouteCollection();
+    }
+
+    private function loadAllDefinitions(mixed $resource): RouteCollection
+    {
         $routes = new RouteCollection();
-
         foreach ($this->definitionManager->getDefinitions() as $definition) {
-            foreach ($definition::getCapabilities() as $capability) {
-                if ($capability instanceof Page) {
-                    $route = new Route(
-                        '/' . $definition::getRoutePathPrefix() . '/',
-                        [
-                            '_resource' => $resource,
-                            '_controller' => $definition::getController() . '::' . $capability->toRoute() . 'Action',
-                        ]
-                    );
+            $this->addDefinitionRoutes($routes, $definition, $resource, true);
+        }
+        return $routes;
+    }
 
-                    switch ($capability) {
-                        case Page::INDEX:
-                            break;
-                        case Page::SHOW:
-                            $route->setPath($route->getPath() . '{id}');
-                            $route->setRequirement('id', '\d+');
-                            break;
-                        case Page::RELOAD:
-                            $route->setPath($route->getPath() . '{id}/reload/{block}/{field?}');
-                            $route->setRequirement('id', '\d+');
-                            $route->setRequirement('block', '\w+');
-                            $route->setRequirement('field', '(\w|\.)+');
-                            break;
-                        case Page::CREATE:
-                            $route->setPath($route->getPath() . 'create');
-                            $route->setMethods(['GET', 'POST']);
-                            break;
-                        case Page::CREATEMODAL:
-                            $route->setPath($route->getPath() . 'createmodal');
-                            $route->setMethods(['GET', 'POST']);
-                            break;
-                        case Page::EDIT:
-                            $route->setPath($route->getPath() . '{id}/edit');
-                            $route->setMethods(['GET', 'POST', 'PUT', 'PATCH']);
-                            $route->setRequirement('id', '\d+');
-                            break;
-                        case Page::DELETE:
-                            $route->setPath($route->getPath() . '{id}/delete');
-                            $route->setMethods(['POST']);
-                            $route->setRequirement('id', '\d+');
-                            break;
-                        case Page::BATCH:
-                            $route->setPath($route->getPath() . 'batch');
-                            $route->setMethods(['POST']);
-                            break;
-                        case Page::EXPORT:
-                            $route->setPath($route->getPath() . 'export');
-                            $route->setMethods(['GET']);
-                            break;
-                        case Page::AJAXFORM:
-                            $route->setPath($route->getPath() . 'ajax-form');
-                            $route->setMethods(['POST']);
-                            break;
-                        case Page::JSONSEARCH:
-                            $route->setPath($route->getPath() . 'json-search');
-                            $route->setMethods(['GET']);
-                            break;
-                    }
+    private function loadDefinition(string $definitionClass, mixed $resource): RouteCollection
+    {
+        $routes = new RouteCollection();
+        $this->addDefinitionRoutes($routes, $definitionClass, $resource, false);
+        return $routes;
+    }
 
-                    $routes->add($definition::getRoutePrefix() . '_' . $capability->toRoute(), $route);
+    private function addDefinitionRoutes(RouteCollection $routes, string $definition, mixed $resource, bool $usePathPrefix = true): void
+    {
+        foreach ($definition::getCapabilities() as $capability) {
+            if ($capability instanceof Page) {
+                $basePath = $usePathPrefix ? '/' . $definition::getRoutePathPrefix() . '/' : '/';
+                $route = new Route(
+                    $basePath,
+                    [
+                        '_resource' => $resource,
+                        '_controller' => $definition::getController() . '::' . $capability->toRoute() . 'Action',
+                    ]
+                );
+
+                switch ($capability) {
+                    case Page::INDEX:
+                        break;
+                    case Page::SHOW:
+                        $route->setPath($route->getPath() . '{id}');
+                        $route->setRequirement('id', '\d+');
+                        break;
+                    case Page::RELOAD:
+                        $route->setPath($route->getPath() . '{id}/reload/{block}/{field?}');
+                        $route->setRequirement('id', '\d+');
+                        $route->setRequirement('block', '\w+');
+                        $route->setRequirement('field', '(\w|\.)+');
+                        break;
+                    case Page::CREATE:
+                        $route->setPath($route->getPath() . 'create');
+                        $route->setMethods(['GET', 'POST']);
+                        break;
+                    case Page::CREATEMODAL:
+                        $route->setPath($route->getPath() . 'createmodal');
+                        $route->setMethods(['GET', 'POST']);
+                        break;
+                    case Page::EDIT:
+                        $route->setPath($route->getPath() . '{id}/edit');
+                        $route->setMethods(['GET', 'POST', 'PUT', 'PATCH']);
+                        $route->setRequirement('id', '\d+');
+                        break;
+                    case Page::DELETE:
+                        $route->setPath($route->getPath() . '{id}/delete');
+                        $route->setMethods(['POST']);
+                        $route->setRequirement('id', '\d+');
+                        break;
+                    case Page::BATCH:
+                        $route->setPath($route->getPath() . 'batch');
+                        $route->setMethods(['POST']);
+                        break;
+                    case Page::EXPORT:
+                        $route->setPath($route->getPath() . 'export');
+                        $route->setMethods(['GET']);
+                        break;
+                    case Page::AJAXFORM:
+                        $route->setPath($route->getPath() . 'ajax-form');
+                        $route->setMethods(['POST']);
+                        break;
+                    case Page::JSONSEARCH:
+                        $route->setPath($route->getPath() . 'json-search');
+                        $route->setMethods(['GET']);
+                        break;
                 }
+
+                $routes->add($definition::getRoutePrefix() . '_' . $capability->toRoute(), $route);
             }
         }
-
-        $this->isLoaded = true;
-
-        return $routes;
     }
 
     /**
@@ -103,6 +126,6 @@ class CrudLoader extends Loader
      */
     public function supports(mixed $resource, ?string $type = null): bool
     {
-        return $type === 'whatwedo_crud';
+        return $type === 'whatwedo_crud' || $type === 'crud';
     }
 }
